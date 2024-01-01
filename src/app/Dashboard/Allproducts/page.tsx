1ca6f5cab3b09e9b64ca922ce00be5734cd0a0bf
mrsayed2022 <am6776756@gmail.com>
2024-01-01 22:10:43 +0200

    first commit

--- src/app/Dashboard/Allproducts/page.tsx
+++ src/app/Dashboard/Allproducts/page.tsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer, toast } from 'react-toastify';
 function Page() {
-    const [starImages, setStarImages] = useState([]);
+  const [starImages, setStarImages] = useState<StaticImageData[]>([]);
 
     
 const toastify=()=>{
